be5efd738bca008ac02831832b3b3ed9a21efb93
SleoNiiX <cardosomoreira.anthony@gmail.com>
2023-10-13 16:34:12 +0200

    Win_detect

--- main.py
+++ main.py
@@ -10,10 +10,42 @@ def next_turn(row,column):
     if button[row][column]['text'] == '':
         button[row][column]['text'] = player
 
+        if win_detect() == False:
+            print('non')
+        elif win_detect() == True:
+            print('gg')
+        elif win_detect() == 'no_match':
+            print('no_match')            
+
         if player == players[1]:
             player = players[0]
         else:
             player = players[1]
+
+def win_detect():
+    for row in range(3):
+        if button[row][0]['text'] == button[row][1]['text'] == button[row][2]['text'] != '':
+            return True
+
+    for column in range(3):
+        if button[0][column]['text'] == button[1][column]['text'] == button[2][column]['text'] != '':
+            return True
+        
+    if button[0][0]['text'] == button[1][1]['text'] == button[2][2]['text'] != '':
+        return True
+
+    elif button[0][2]['text'] == button[1][1]['text'] == button[2][0]['text'] != '':
+        return True
+    
+    return no_match()
+
+def no_match():
+    for row in range(3):
+        for column in range(3):
+            if button[row][column]['text'] == '' :
+                return False
+    return 'no_match'
+
 
 window = Tk()
 window.title("Tic Tac Toe")
